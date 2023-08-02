4b450bfdeae20633d0a69d5cf6f5556d3c6627df
Liu Changxi <changxi_liu@u.nus.edu>
2023-08-02 16:22:45 +0800

    [MICRO2023]: artifact evaluation

---
+++ dataset/mnist/data/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz \
+    --output train-images-idx3-ubyte.gz
+curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz \
+    --output train-labels-idx1-ubyte.gz 
+curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz \
+    --output t10k-images-idx3-ubyte.gz 
+curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz \
+    --output t10k-labels-idx1-ubyte.gz 
---
+++ tensor/tensor.go
@@ -0,0 +1,90 @@
+// Package tensor defines the tensor interface.
+package tensor
+import (
+    "fmt"
+)
+
+// A Tensor is a multi-dimension matrix.
+type Tensor interface {
+	// Dim returns the number of dimension of the tensor
+	Dim() int
+	PrintDim() 
+
+	// NumElement returns the total number of elements in the tensor
+	NumElement() int
+
+	// Size returns the length of the tensor in each dimension, from the
+	// outermost dimension to the innermost dimension.
+	Size() []int
+
+	// SetSize sets the size of the tensor.
+	SetSize([]int)
+
+	// Vector returns the data of the tensor represented in a pure vector.
+	// Here, we use float64. However, the concrete tensor implementation can
+	// use lower-precision numbers.
+	Vector() []float64
+
+	// Descriptor represents what does each dimension of the tensor represents.
+	//
+	// Tokens that represent the meaning of the dimensions include N, C, H, W.
+	Descriptor() string
+
+	// SetDescriptor sets the descriptor of the tensor.
+	SetDescriptor(d string)
+}
+
+// A SimpleTensor is a multi-dimensional matrix.
+type SimpleTensor struct {
+	size       []int
+	data       []float64
+	descriptor string
+}
+
+// Dim returns the number of dimensions that the tensor has.
+func (t SimpleTensor) Dim() int {
+	return len(t.size)
+}
+
+// NumElement returns the total number of elements in the tensor.
+func (t SimpleTensor) NumElement() int {
+	n := 1
+
+	for _, s := range t.size {
+		n *= s
+	}
+
+	return n
+}
+
+// Size returns the size of the tensor.
+func (t SimpleTensor) Size() []int {
+	return t.size
+}
+func (t SimpleTensor) PrintDim()  {
+    for _,elem := range(t.size) {
+      fmt.Printf("%d ",elem)
+    }
+    fmt.Printf("\n")
+}
+
+
+// SetSize sets the size of the tensor
+func (t *SimpleTensor) SetSize(newSize []int) {
+	t.size = newSize
+}
+
+// Vector returns the raw data of the tensor.
+func (t SimpleTensor) Vector() []float64 {
+	return t.data
+}
+
+// Descriptor returns the descriptor of the tensor.
+func (t SimpleTensor) Descriptor() string {
+	return t.descriptor
+}
+
+// SetDescriptor sets the descriptor of the tensor
+func (t *SimpleTensor) SetDescriptor(d string) {
+	t.descriptor = d
+}
